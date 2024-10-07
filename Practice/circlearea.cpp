b1dfafd73b0d37097a57208558a6a874b434f01a
nadim249 <mahmoudnadim47@gmail.com>
2024-10-07 22:11:27 +0600

    Doubly LL arr to dll and delete

---
+++ Practice/circlearea.cpp
@@ -0,0 +1,14 @@
+//cos π = −1
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int r;
+    cin>>r;
+    double carea=0;
+    double pi=acos(-1);
+
+    carea=pi*r*r;
+
+    cout<<"Circle area is "<<carea;
+
+}
